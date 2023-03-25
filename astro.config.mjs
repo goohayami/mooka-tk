beeb5f45c57ad0cc7e08c2560df6148d44203490
goohayami <goohayami@gmail.com>
2023-03-25 18:25:36 +0900

    mooka2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://goohayami.mooka.github.io",
-  base: "mooka",
+  site: "https://goohayami.github.io",
+  base: "/mooka",
 });
